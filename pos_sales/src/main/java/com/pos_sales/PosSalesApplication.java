2869de53c90cd91cec89a7c2fc9744fda4631aa0
Trisha Joy Oballo <116042598+Trissarious@users.noreply.github.com>
2024-02-01 21:16:14 +0800

    Update PosSalesApplication.java

--- pos_sales/src/main/java/com/pos_sales/PosSalesApplication.java
+++ pos_sales/src/main/java/com/pos_sales/PosSalesApplication.java
@@ -3,8 +3,10 @@ package com.pos_sales;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(CorsConfig.class)
 public class PosSalesApplication {
 
 	@Autowired
