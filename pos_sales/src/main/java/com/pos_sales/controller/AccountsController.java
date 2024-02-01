14f0b5937dfdac026500e0114475d8e1426a0121
Trisha Joy Oballo <116042598+Trissarious@users.noreply.github.com>
2024-02-01 18:18:52 +0800

    for deployment

--- pos_sales/src/main/java/com/pos_sales/controller/AccountsController.java
+++ pos_sales/src/main/java/com/pos_sales/controller/AccountsController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.UUID;
 
 
-@CrossOrigin("https://dilven.vercel.app")
+@CrossOrigin(origins = "https://dilven.vercel.app")
 @RestController
 @RequestMapping("/user")
 public class AccountsController {
