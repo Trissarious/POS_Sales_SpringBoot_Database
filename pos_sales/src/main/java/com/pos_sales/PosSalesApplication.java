e8010f5bab52f5a6edd1bd1ce87a0df858482f27
Trisha Joy Oballo <116042598+Trissarious@users.noreply.github.com>
2024-02-01 20:39:06 +0800

    Update PosSalesApplication.java

--- pos_sales/src/main/java/com/pos_sales/PosSalesApplication.java
+++ pos_sales/src/main/java/com/pos_sales/PosSalesApplication.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -40,17 +39,11 @@ public class PosSalesApplication {
 			};
 		}
 
-	@Configuration
 	@EnableWebSecurity
-	public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+	public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable()
-					.authorizeRequests()
-					.antMatchers("/**").permitAll() // Allow login URL without authentication
-					.anyRequest().authenticated()
-					.and().cors(); // Enable CORS
+			http.cors().and().csrf().disable();
 		}
 	}
 
