43f8f322b0d44e721b5c1607dc6e194cf691aedf
Trisha Joy Oballo <116042598+Trissarious@users.noreply.github.com>
2024-02-01 16:02:43 +0800

    Update AccountsController.java

--- pos_sales/src/main/java/com/pos_sales/controller/AccountsController.java
+++ pos_sales/src/main/java/com/pos_sales/controller/AccountsController.java
@@ -190,7 +190,7 @@ public class AccountsController {
 				    try {
 				        helper.setTo(toEmail);
 				        helper.setSubject("Password Reset");
-				        helper.setText("<p>Click <a href='http://localhost:3000/changepassword?token=" + resetToken + "'>this link</a> to reset your password</p>  <p> This link will expire in 24 hours.", true);
+				        helper.setText("<p>Click <a href='https://dilven.vercel.app/changepassword?token=" + resetToken + "'>this link</a> to reset your password</p>  <p> This link will expire in 24 hours.", true);
 
 				        javaMailSender.send(message);
 				        
@@ -207,7 +207,7 @@ public class AccountsController {
 			    	return UUID.randomUUID().toString();
 			    }
 			    
-			    @CrossOrigin(origins = "http://localhost:3000")
+			    @CrossOrigin(origins = "https://dilven.vercel.app/")
 			  //Update a record
 				@PutMapping("/changepassword")
 				public AccountsModel ChangePassword(@RequestParam String resetToken, @RequestBody AccountsModel newAccountsDetails) throws Exception{
