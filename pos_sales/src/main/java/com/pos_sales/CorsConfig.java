34b0cf5f25cd1a07d2a9b41ddfcba1c74865eb98
Trisha Joy Oballo <116042598+Trissarious@users.noreply.github.com>
2024-02-02 12:21:05 +0800

    t

--- pos_sales/src/main/java/com/pos_sales/CorsConfig.java
+++ pos_sales/src/main/java/com/pos_sales/CorsConfig.java
@@ -2,10 +2,10 @@ package com.pos_sales;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+////import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -14,15 +14,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 public class CorsConfig {
     
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-    	http.cors().and().csrf().disable().authorizeRequests().antMatchers("/**").permitAll();
-    	return http.build();
-    	
-    }
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+//    	http.cors().and().csrf().disable().authorizeRequests().antMatchers("/**").permitAll();
+//    	return http.build();
+//    	
+//    }
 	
 //	@Bean
 //	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
