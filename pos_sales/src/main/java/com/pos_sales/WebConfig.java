22b12b511c89ed51c0d8aad03e75c5d15d6c7114
Trisha Joy Oballo <116042598+Trissarious@users.noreply.github.com>
2024-02-02 13:28:34 +0800

    dsa

--- pos_sales/src/main/java/com/pos_sales/WebConfig.java
+++ pos_sales/src/main/java/com/pos_sales/WebConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
