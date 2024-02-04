64a60d1fdd6645ebaaf9744701520383d8eee7b6
Trisha Joy Oballo <116042598+Trissarious@users.noreply.github.com>
2024-02-04 13:30:24 +0800

    Update CORSFilter.java

--- pos_sales/src/main/java/com/pos_sales/CORSFilter.java
+++ pos_sales/src/main/java/com/pos_sales/CORSFilter.java
@@ -35,6 +35,7 @@
 
             response.setHeader("Access-Control-Allow-Origin", "*");
             response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers");
             response.setHeader("Access-Control-Max-Age", "3600");
             response.setHeader("Access-Control-Allow-Credentials", "true");
 
